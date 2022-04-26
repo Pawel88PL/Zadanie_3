183c9fea9711303e059ad6b628af56c3b3913561
Pawel Staniul <pstaniul@gmail.com>
2022-04-26 19:25:23 +0200

    Opis programu, zadeklarowanie zmiennych i przypisanie im wartości pobranych od użytkownika.

--- Zadanie_3/Program.cs
+++ Zadanie_3/Program.cs
@@ -10,7 +10,26 @@ namespace Zadanie_3
     {
         static void Main(string[] args)
         {
+            
+            Console.WriteLine("Program oblicza największy wspólny dzielnik dwóch dodatnich liczb całkowitych.");
+            Console.WriteLine("Wykorzystano algorytm Euklidesa nie używając operacji dzielenia.");
+            Console.WriteLine();
 
+            int firstNumber, secondNumber, commonDivisor;
+
+            commonDivisor = 0;
+
+            Console.Write("Podaj pierwszą liczbę: ");
+            firstNumber = int.Parse(Console.ReadLine());
+
+            Console.Write("Podaj drugą liczbę: ");
+            secondNumber = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine();
+            Console.WriteLine($"Największy wspólny dzielnik liczb ({firstNumber}, {secondNumber}) wynosi: " + commonDivisor);
+
+            Console.ReadLine();
         }
     }
 }
